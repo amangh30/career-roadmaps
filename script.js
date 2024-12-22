67f6e8a15eed3054a39c5d9d09bcb2a171e11767
SoumyajitPr <prsoumyajit@gmail.com>
2024-12-22 12:30:13 +0530

    update

--- script.js
+++ script.js
@@ -103,11 +103,11 @@ document.addEventListener('DOMContentLoaded', () => {
         if (body.classList.contains('day-mode')) {
             body.classList.remove('day-mode');
             body.classList.add('night-mode');
-            toggleButton.textContent = '☀️ Day Mode';
+            toggleButton.textContent = '☀️';
         } else {
             body.classList.remove('night-mode');
             body.classList.add('day-mode');
-            toggleButton.textContent = '🌙 Night Mode';
+            toggleButton.textContent = '🌙';
         }
     });
 });
